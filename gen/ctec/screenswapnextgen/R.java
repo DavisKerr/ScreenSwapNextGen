d77a18ca5fef2daa216c94cf0eaeb04d4a36fdc5
DavisKerr <davis.kerr1997@gmail.com>
2014-10-20 13:37:34 -0600

    Added more stuff

--- gen/ctec/screenswapnextgen/R.java
+++ gen/ctec/screenswapnextgen/R.java
@@ -1078,10 +1078,12 @@ containing a value of this type.
         public static final int up=0x7f02005a;
     }
     public static final class id {
+        public static final int Canvas=0x7f05003c;
         public static final int DownButton=0x7f05003e;
         public static final int HomeButton=0x7f050042;
         public static final int LeftButton=0x7f05003f;
         public static final int NextButton=0x7f050043;
+        public static final int Player1=0x7f050041;
         public static final int RightButton=0x7f050040;
         public static final int UpButton=0x7f05003d;
         public static final int action_bar=0x7f05001c;
@@ -1113,8 +1115,6 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-        public static final int imageView1=0x7f05003c;
-        public static final int imageView2=0x7f050041;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
--- src/ctec/screenswapnextgen/controller/ScreenThirdActivity.java
+++ src/ctec/screenswapnextgen/controller/ScreenThirdActivity.java
@@ -3,9 +3,11 @@ package ctec.screenswapnextgen.controller;
 import ctec.screenswapnextgen.R;
 import ctec.screenswapnextgen.R.layout;
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class ScreenThirdActivity extends Activity
@@ -14,6 +16,7 @@ public class ScreenThirdActivity extends Activity
 	private Button downButton;
 	private Button leftButton;
 	private Button rightButton;
+	private Drawable canvas; 
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -24,6 +27,7 @@ public class ScreenThirdActivity extends Activity
 		downButton = (Button) findViewById(R.id.DownButton);
 		leftButton = (Button) findViewById(R.id.LeftButton);
 		rightButton = (Button) findViewById(R.id.RightButton);
+		canvas = (Drawable) findViewById(R.id.Player1);
 		
 		setUpListeners();
 		
@@ -31,6 +35,17 @@ public class ScreenThirdActivity extends Activity
 	
 	private void setUpListeners()
 	{
+		
+		upButton.setOnClickListener(new OnClickListener()
+		{
+			
+			@Override
+			public void onClick(View, )
+			{
+				
+			}
+			
+		});
 		
 	}
 }
